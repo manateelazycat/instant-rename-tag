9b2cf01e8ed0352e2f1bdb35ba5ab089da021992
Andy Stewart <lazycat.manatee@gmail.com>
2019-06-26 23:40:55 +0800

    first post

---
+++ README.md
@@ -0,0 +1,23 @@
+<img src="./screenshot/instant-rename-tag.gif">
+
+# What is instant-rename-tag?
+It's wonderful if we can rename tag instantly, not rename from minibuffer.
+And yes, this plugin is design for do this.
+
+## Installation
+Clone or download this repository (path of the folder is the `<path-to-instant-rename-tag>` used below).
+
+In your `~/.emacs`, add the following two lines:
+```Elisp
+(add-to-list 'load-path "<path-to-instant-rename-tag>") ; add instant-rename-tag to your load-path
+(require 'instant-rename-tag)
+```
+
+Note, this plugin depend on ```web-mode```, you need make sure install ```web-mode``` first.
+
+## Usage
+Bind your favorite key to functions:
+
+| Function           | Description                     |
+| :--------          | :----                           |
+| instant-rename-tag | Rename tag under current cursor |
---
+++ instant-rename-tag.el
@@ -0,0 +1,237 @@
+;;; instant-rename-tag.el --- Instant rename tag
+
+;; Filename: instant-rename-tag.el
+;; Description: Instant rename tag
+;; Author: Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2019, Andy Stewart, all rights reserved.
+;; Created: 2019-03-14 22:14:00
+;; Version: 0.1
+;; Last-Updated: 2019-03-14 22:14:00
+;;           By: Andy Stewart
+;; URL: http://www.emacswiki.org/emacs/download/instant-rename-tag.el
+;; Keywords:
+;; Compatibility: GNU Emacs 26.1.92
+;;
+;; Features that might be required by this library:
+;;
+;; `web-mode'
+;;
+
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; It's wonderful if we can rename tag instantly, not rename from minibuffer.
+;; And yes, this plugin is design for do this.
+;;
+
+;;; Installation:
+;;
+;; Put instant-rename-tag.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'instant-rename-tag)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;;
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET instant-rename-tag RET
+;;
+
+;;; Change log:
+;;
+;; 2019/06/26
+;;      * Use overlay re-implement code.
+;;
+;; 2019/03/14
+;;      * First released.
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
+(require 'web-mode)
+
+;;; Code:
+
+(defgroup instant-rename-tag nil
+  "Instant rename tag."
+  :group 'instant-rename-tag)
+
+(defface instant-rename-tag-mark
+  '((t (:foreground "White" :background "#007aff" :bold t)))
+  "Face tag."
+  :group 'instant-rename-tag)
+
+(defun instant-rename-tag ()
+  (interactive)
+  (cond ((instant-rename-tag-in-open-tag-p)
+         (if (and (boundp 'instant-rename-is-mark)
+                  instant-rename-is-mark)
+             (instant-rename-unmark-tag)
+           (instant-rename-mark-tag)))
+        ((instant-rename-tag-in-close-tag-p)
+         (if (and (boundp 'instant-rename-is-mark)
+                  instant-rename-is-mark)
+             (instant-rename-unmark-tag)
+           (instant-rename-mark-tag)))
+        (t
+         (message "Not in tag area."))))
+
+(defun instant-rename-mark-tag ()
+  (ignore-errors
+    (let* ((open-tag-pos (save-excursion
+                           (web-mode-element-beginning)
+                           (point)))
+           (start-pos (save-excursion
+                        (goto-char open-tag-pos)
+                        (forward-char 1)
+                        (point)))
+           (end-pos (save-excursion
+                      (goto-char start-pos)
+                      (unless (looking-at ">")
+                        (forward-symbol 1))
+                      (point))))
+      (when (and start-pos end-pos)
+        (set (make-local-variable 'instant-rename-open-tag-overlay) (make-overlay start-pos end-pos))
+        (overlay-put instant-rename-open-tag-overlay 'face 'instant-rename-tag-mark)
+        )))
+
+  (ignore-errors
+    (let* ((close-tag-pos (save-excursion
+                            (instant-rename-tag-jump-to-match)
+                            (point)))
+           (start-pos (save-excursion
+                        (goto-char close-tag-pos)
+                        (forward-char 2)
+                        (point)))
+           (end-pos (save-excursion
+                      (goto-char start-pos)
+                      (unless (looking-at ">")
+                        (forward-symbol 1))
+                      (point))))
+      (when (and start-pos end-pos)
+        (set (make-local-variable 'instant-rename-close-tag-overlay) (make-overlay start-pos end-pos))
+        (overlay-put instant-rename-close-tag-overlay 'face 'instant-rename-tag-mark)
+        )))
+
+  (set (make-local-variable 'instant-rename-is-mark) t)
+  )
+
+(defun instant-rename-unmark-tag ()
+  (when instant-rename-open-tag-overlay
+    (delete-overlay instant-rename-open-tag-overlay)
+    (set (make-local-variable 'instant-rename-open-tag-overlay) nil))
+
+  (when instant-rename-close-tag-overlay
+    (delete-overlay instant-rename-close-tag-overlay)
+    (set (make-local-variable 'instant-rename-close-tag-overlay) nil))
+
+  (set (make-local-variable 'instant-rename-is-mark) nil))
+
+(defun instant-rename-tag-in-open-tag-p ()
+  (let ((open-tag-pos (save-excursion
+                        (sgml-skip-tag-backward 1)
+                        (point))))
+    (and (equal (line-number-at-pos open-tag-pos) (line-number-at-pos (point)))
+         (save-excursion
+           (search-backward-regexp "<" open-tag-pos t))
+         (not (save-excursion
+                (search-backward-regexp "\\s-" open-tag-pos t))))))
+
+(defun instant-rename-tag-in-close-tag-p ()
+  (let ((close-tag-pos (save-excursion
+                         (instant-rename-tag-jump-to-match)
+                         (point))))
+    (and (equal (line-number-at-pos close-tag-pos) (line-number-at-pos (point)))
+         (save-excursion
+           (search-backward-regexp "</" close-tag-pos t))
+         (not (save-excursion
+                (search-backward-regexp "\\s-" close-tag-pos t))))))
+
+(defun instant-rename-tag-jump-to-match ()
+  (web-mode-element-beginning)
+  (cond ((looking-at "<>")
+         (web-mode-tag-match))
+        (t
+         (sgml-skip-tag-forward 1)))
+  (search-backward-regexp "</"))
+
+(defun instant-rename-tag-after-change-function (begin end length)
+  (when (and
+         (derived-mode-p 'web-mode)
+         (boundp 'instant-rename-is-mark)
+         instant-rename-is-mark
+         instant-rename-open-tag-overlay
+         instant-rename-close-tag-overlay
+         )
+    (let* ((disable-company-mode (when (featurep 'company-mode)
+                                   (company-mode -1)))
+           (open-tag-start-pos (overlay-start instant-rename-open-tag-overlay))
+           (open-tag-end-pos (overlay-end instant-rename-open-tag-overlay))
+           (close-tag-start-pos (overlay-start instant-rename-close-tag-overlay))
+           (close-tag-end-pos (overlay-end instant-rename-close-tag-overlay)))
+      (cond ((and (>= (point) open-tag-start-pos)
+                  (<= (point) (+ 1 open-tag-end-pos)))
+             (let ((new-tag (buffer-substring open-tag-start-pos (max open-tag-end-pos (point)))))
+               (save-excursion
+                 (delete-region close-tag-start-pos close-tag-end-pos)
+                 (goto-char close-tag-start-pos)
+                 (insert new-tag)
+                 (move-overlay instant-rename-open-tag-overlay open-tag-start-pos (+ open-tag-start-pos (length new-tag)))
+                 (move-overlay instant-rename-close-tag-overlay (- (point) (length new-tag)) (point))
+                 )))
+            ((and (>= (point) close-tag-start-pos)
+                  (<= (point) (+ 1 close-tag-end-pos)))
+             (let* ((open-tag (buffer-substring open-tag-start-pos open-tag-end-pos))
+                    (current-point (max close-tag-end-pos (point)))
+                    (new-tag (buffer-substring close-tag-start-pos current-point))
+                    (tag-offset (- (length new-tag) (length open-tag)))
+                    (close-tag-new-start-pos (+ close-tag-start-pos tag-offset)))
+               (save-excursion
+                 (delete-region open-tag-start-pos open-tag-end-pos)
+                 (goto-char open-tag-start-pos)
+                 (insert new-tag)
+                 (move-overlay instant-rename-close-tag-overlay close-tag-new-start-pos (+ close-tag-new-start-pos (length new-tag)))
+                 (move-overlay instant-rename-open-tag-overlay (- (point) (length new-tag)) (point))))
+             )))))
+
+(add-hook 'after-change-functions #'instant-rename-tag-after-change-function)
+
+(provide 'instant-rename-tag)
+
+;;; instant-rename-tag.el ends here
